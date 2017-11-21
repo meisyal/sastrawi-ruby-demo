6945187ea781dadfcaa67f3a29e1e03a5f099ca0
Andrias Meisyal <andriasonline@gmail.com>
2017-11-21 22:26:56 +0700

    Add a route to stem inputted text

--- app.rb
+++ app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'sastrawi'
 
 get '/' do
   erb :index
+end
+
+post '/' do
+  stemmer_factory = Sastrawi::Stemmer::StemmerFactory.new
+  stemmer = stemmer_factory.create_stemmer
+  stemming_result = stemmer.stem(params['input'])
+
+  "Stemming result: #{stemming_result}"
 end
