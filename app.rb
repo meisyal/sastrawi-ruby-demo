555d8c98608d02fde5fbdeb6dad4544804d83b6d
Andrias Meisyal <andriasonline@gmail.com>
2017-12-01 18:01:13 +0700

    Separate stemming process into a function

--- app.rb
+++ app.rb
@@ -1,14 +1,20 @@
-require 'sinatra'
 require 'sastrawi'
+require 'sinatra'
 
 get '/' do
   erb :index
 end
 
 post '/' do
-  stemmer_factory = Sastrawi::Stemmer::StemmerFactory.new
-  stemmer = stemmer_factory.create_stemmer
-  stemming_result = stemmer.stem(params['input'])
+  stemming_result = stem(params['input'])
 
   "Stemming result: #{stemming_result}"
+end
+
+def stem(text)
+  stemmer_factory = Sastrawi::Stemmer::StemmerFactory.new
+  stemmer = stemmer_factory.create_stemmer
+  stemming_result = stemmer.stem(text)
+
+  stemming_result
 end
