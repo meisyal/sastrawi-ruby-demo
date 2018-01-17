c04ebe600a04e73bc01a811a5aecbc92fff020d2
Andrias Meisyal <andriasonline@gmail.com>
2018-01-17 21:46:23 +0700

    Handle long text

--- app.rb
+++ app.rb
@@ -16,13 +16,17 @@ end
 post '/' do
   @text = params['input']
 
-  start_time = Time.now
-  stemming_result = stem(@text)
-  end_time = Time.now
+  if @text.length > 3500
+    erb :index, :locals => {:result => "Text is too long, please download the offline version of sastrawi-ruby instead.", :time => nil}
+  else
+    start_time = Time.now
+    stemming_result = stem(@text)
+    end_time = Time.now
 
-  elapsed_time = end_time - start_time
+    elapsed_time = end_time - start_time
 
-  erb :index, :locals => {:result => stemming_result, :time => elapsed_time}
+    erb :index, :locals => {:result => stemming_result, :time => elapsed_time}
+  end
 end
 
 def stem(text)
