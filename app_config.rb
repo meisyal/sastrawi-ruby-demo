5753f91510e59d2239322843b02de5a57f359a82
Andrias Meisyal <andriasonline@gmail.com>
2018-03-21 22:47:09 +0700

    Add sastrawi gem download link

--- app_config.rb
+++ app_config.rb
@@ -1,5 +1,6 @@
 # General
-OFFLINE_VERSION_INFO = "Text is too long, please download the offline version of sastrawi-ruby instead."
+SASTRAWI_RUBYGEMS = "https://rubygems.org/gems/sastrawi"
+OFFLINE_VERSION_INFO = "Text is too long, please download the offline version of sastrawi-ruby instead. Link #{SASTRAWI_RUBYGEMS}"
 
 # Text
 MAX_TEXT_LONG_CHARACTER = 2500
